54ebd220dbfd3ab11f00ec11c9ef358c3eb05877
Pedro Barros <pedrobarros0101@outlook.com>
2022-07-13 21:05:27 -0300

    [FIX][BACK END CLUBE] - Alteração do pool do banco de dados

--- postgre.js
+++ postgre.js
@@ -1,2 +1,10 @@
-const pool = require('pg');
+const pg = require('pg');
+
+const pool = pg.Pool({
+    user: process.env.POSTGRE_USER,
+    host: process.env.POSTGRE_HOST,
+    database: process.env.POSTGRE_DATABASE,
+    password: process.env.POSTGRE_PASSWORD,
+    port: process.env.POSTGRE_PORT
+});
 
