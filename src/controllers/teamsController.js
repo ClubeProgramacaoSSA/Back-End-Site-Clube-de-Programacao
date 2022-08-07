42196741ece582d60725cd0699281d6dad7a154e
FacundesPedro <pedro.f.facundes1@gmail.com>
2022-08-06 21:02:17 -0300

    [refactor]: refatoring pg service

--- src/controllers/teamsController.js
+++ src/controllers/teamsController.js
@@ -1,7 +1,7 @@
-import postgre from '../service/postgre';
+import { executeQuerySql } from '../service/postgre';
 export const getTeams = async (req, res, next) => {
     try{
-        const responseData = await postgre.executeQuerySql("SELECT * FROM TB_EQUIPE;");
+        const responseData = await executeQuerySql("SELECT * FROM TB_EQUIPE;");
         return res.status(200).send({response: responseData.rows});
     }catch(error){
         return res.status(500).send({error: error});
@@ -10,7 +10,7 @@ export const getTeams = async (req, res, next) => {
 export const getTeam = async (req, res, next) => {
     const id_team = req.params.id_equipe;
     try {
-        const responseData = await postgre.executeQuerySql(
+        const responseData = await executeQuerySql(
             "SELECT * FROM TB_EQUIPE WHERE ID_EQUIPE = $1", 
             [id_team]);
         return res.status(200).send({response: responseData.rows[0]});
