7a3096caa7844c9e5c502847e176a63650044ca6
FacundesPedro <pedro.f.facundes1@gmail.com>
2022-08-25 11:51:08 -0300

    [refactor]: added deleteTeam to its router

--- src/routes/team.js
+++ src/routes/team.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import {getTeam,getTeams,postTeam} from '../controllers/teamController.js';
+import {getTeam,getTeams,postTeam,deleteTeam} from '../controllers/teamController.js';
 
 const teamRouter = Router();
 
 teamRouter.post('/', postTeam);
 teamRouter.get('/', getTeams);
 teamRouter.get('/:id_equipe', getTeam);
+teamRouter.delete('/:id',deleteTeam )
 
 export { teamRouter };
