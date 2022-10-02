9bb39bb5b4512f6fae6c2c7a9f70cba9e2697fb7
FacundesPedro <pedro.f.facundes1@gmail.com>
2022-10-02 20:00:43 -0300

    [refactor]: using NODE_ENV

--- src/Db/knex.ts
+++ src/Db/knex.ts
@@ -2,9 +2,9 @@ import 'dotenv/config';
 import { knex } from 'knex';
 import knexConfig from '../../knexfile';
 
-const NODE_ENV = process.env.TYPE_ENV;
+const NODE_ENV = process.env.NODE_ENV;
 
-// if(!NODE_ENV) throw Error('No env type selected!')
+if(!NODE_ENV) throw Error('No env type selected!')
 
-export const foo = () => console.log( knexConfig[ "development" ] );
-export const connection = knex( knexConfig[ "development" ] );
+export const foo = () => console.log( knexConfig[ NODE_ENV ] );
+export const connection = knex( knexConfig[ NODE_ENV ] );
