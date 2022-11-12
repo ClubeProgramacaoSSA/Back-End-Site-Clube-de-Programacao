46dafb35d3a907902df0947a154661b4f8791916
orlandomotapires <orlandomota2002@gmail.com>
2022-11-12 18:38:58 -0300

    [HOTFIX]
    Co-authored-by FernandoSchett fernandoschettini@gmail.com

--- src/modules/projects/routes/index.ts
+++ src/modules/projects/routes/index.ts
@@ -16,73 +16,59 @@ class ProjectRoutes implements Route {
 	public initRoute() {
 		this.router.get('/', this.getAllProjects);
 		this.router.get('/:id_project', this.getEspecificProject);
-		this.router.post('/postTeam', this.postEspecificProject);
-        this.router.put('/putTeam', this.updateEspecificProject);
-        this.router.delete('/delete/:id_member', this.deleteEspecificProject);
+		this.router.post('/postProject', this.postEspecificProject);
+        this.router.put('/putProject', this.updateEspecificProject);
+        this.router.delete('/delete/:id_project', this.deleteEspecificProject);
 
-        this.router.get('/', this.getAllWorkshop );
-		this.router.get('/:id_workshop', this.getEspecificWorkshop);
-		this.router.post('/postWorkshop', this.postEspecificWorkshop);
-        this.router.put('/putWorkshop', this.updateEspecificWorkshop);
-        this.router.delete('/delete/:id_workshop', this.deleteEspecificWorkshop);
+        this.router.get('/:projectType', this.getProjectPerType);
 	
 		return this.router;
 	}
-    getEspecificProject(arg0: string, getEspecificProject: any) {
-        throw new Error('Method not implemented.');
-    }
-    postEspecificProject(arg0: string, postEspecificProject: any) {
-        throw new Error('Method not implemented.');
-    }
-    updateEspecificProject(arg0: string, updateEspecificProject: any) {
-        throw new Error('Method not implemented.');
-    }
-    deleteEspecificProject(arg0: string, deleteEspecificProject: any) {
-        throw new Error('Method not implemented.');
-    }
-    getAllWorkshop(arg0: string, getAllWorkshop: any) {
-        throw new Error('Method not implemented.');
-    }
-    getEspecificWorkshop(arg0: string, getEspecificWorkshop: any) {
-        throw new Error('Method not implemented.');
-    }
-    postEspecificWorkshop(arg0: string, postEspecificWorkshop: any) {
-        throw new Error('Method not implemented.');
-    }
-    updateEspecificWorkshop(arg0: string, updateEspecificWorkshop: any) {
-        throw new Error('Method not implemented.');
-    }
-    deleteEspecificWorkshop(arg0: string, deleteEspecificWorkshop: any) {
-        throw new Error('Method not implemented.');
-    }
 
     private getAllProjects = (req: Request,res:Response) => {
-		this.service.getAllProject()
+		this.service.getAllProjects()
 			.then( (testJson => res.status(200).json(testJson)) )
 			.catch((errObj) => res.status(500).json(errObj))
 	}
 
-	private getAllMembers = (req: Request, res:Response) => {
-		this.service.getAllMembers()
+    private getEspecificProject = (req: Request,res:Response) => {
+        const { id_project } = req.params;
+
+		this.service.getEspecificProject(parseInt(id_project))
 			.then( (testJson => res.status(200).json(testJson)) )
 			.catch((errObj) => res.status(500).json(errObj))
 	}
-	
-	private getEspecificMember = (req: Request,res:Response) => {
-		const { id_member } = req.params;
 
-		this.service.getEspecificMember( parseInt(id_member) )
-			.then(json => res.status(200).json( json ))
-			.catch( errObj => res.status(500).json( errObj )  )
+    private postEspecificProject = (req: Request,res:Response) => {
+        const  project  = req.body;
+
+		this.service.postEspecificProject(project)
+			.then( (testJson => res.status(200).json(testJson)) )
+			.catch((errObj) => res.status(500).json(errObj))
 	}
 
-	private deleteEspecificMember = (req: Request,res:Response) => {
-		const { id_member } = req.params;
+    private updateEspecificProject = (req: Request,res:Response) => {
+        const  project  = req.body;
+		this.service.updateEspecificProject(project)
+			.then( (testJson => res.status(200).json(testJson)) )
+			.catch((errObj) => res.status(500).json(errObj))
+	}
 
-		this.service.deleteEspecificMember( parseInt(id_member) )
-			.then(json => res.status(200).json( json ))
-			.catch( errObj => res.status(500).json( errObj )  )
+    private deleteEspecificProject = (req: Request,res:Response) => {
+        const  { id_project }  = req.params;
+
+		this.service.deleteEspecificProject(parseInt(id_project))
+			.then( (testJson => res.status(200).json(testJson)) )
+			.catch((errObj) => res.status(500).json(errObj))
+	}
+
+    private getProjectPerType = (req: Request,res:Response) => {
+        const { projectType } = req.params;
+
+		this.service.getProjectPerType(projectType)
+			.then( (testJson => res.status(200).json(testJson)) )
+			.catch((errObj) => res.status(500).json(errObj))
 	}
 }
 
-export const memberRouter = new MemberRoutes();
+export const projectRouter = new ProjectRoutes();
