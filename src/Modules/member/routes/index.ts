6620bd78b53c93b15a651cca27a1ea10f35d4b76
FacundesPedro <pedro.f.facundes1@gmail.com>
2023-03-03 17:44:21 -0300

    [refactor]: updating mmeber router and services

--- src/Modules/member/routes/index.ts
+++ src/Modules/member/routes/index.ts
@@ -1,32 +1,51 @@
-import { Router, IRouter, Request, Response } from 'express';
+import { Router, IRouter, Request, Response, NextFunction } from 'express';
 import { Route } from '../../../Models';
-import { MemberService } from '../services';
+import { memberService } from '../services';
 import { IMembers } from '../interfaces';
 import { IAuthentication } from '../../auth/interfaces';
 
 class MemberRouter implements Route {
     private router = Router();
 
-    constructor() {}
+    constructor() { }
 
     public initRoute() {
-        this.router.post('/store', this.storeMember );
-
+        this.router.post('/store', this.storeMember);
+        this.router.post('/getBy', this.getMemberBySomething)
         return this.router;
     }
+    private async getMemberBySomething(req: Request, res: Response,next:NextFunction) {
+        const partialMember = req.body as Partial< Omit< IMembers,'id' > >;
+        try {
+            const members = await memberService.getBySomething(partialMember);
+            
+            res.status(200).json({ member: members[0] })
+        } catch(err) {
+            next(new Error('Error getting members'));
+        }
+    }
+    private async getById(req: Request, res: Response) {
 
+    }
     private async storeMember(req: Request, res: Response) {
         // should receive a member from body and its authentication;
         const member = req.body as Omit<IMembers, 'id'>;
         const { fullname, gender, started_course_at, born_at, created_at, description, updated_at } = member;
+        // const nullishFields = [];
 
-        const memberId = await new MemberService().create({ ...member });
-        
-        if( !memberId ) {
-            throw new Error('Error creating member')
+        // Object.entries(member).forEach(( [ key, value ]) => {
+        //     if( !value ) {
+        //         nullishFields.push(key);
+        //     }
+        // });
+
+        const memberId = await memberService.create({ ...member });
+
+        if (!memberId) {
+            throw new Error(`Error storing member`)
         }
-        
-        return res.status(203).json( { memberId: memberId } ); 
+
+        return res.status(201).json({ memberId: memberId });
     }
 
 }
--- src/Modules/member/services/index.ts
+++ src/Modules/member/services/index.ts
@@ -3,7 +3,7 @@ import { connection } from "../../../Db/knex";
 import { IBaseCrudService } from "../../../Models";
 import { IMembers } from '../interfaces';
 
-export class MemberService {
+class MemberService {
     private tableName = 'members';
     private kxConnection: Knex;
 
@@ -11,21 +11,45 @@ export class MemberService {
         this.kxConnection = connection;
     }
 
-    async create (member: IMembers){
+    async create(member: IMembers) {
         // should create member and return it`s Id;
         try {
-            const [ memberWithId ] = await this.kxConnection( this.tableName )
-                    .insert( member,['id']) as Array<{ id: string } | undefined>;
-                    
-            if(memberWithId) {
+            const [memberWithId] = await this.kxConnection(this.tableName)
+                .insert(member, ['id']) as Array<{ id: string } | undefined>;
+
+            if (memberWithId) {
                 console.log(`Member Id`, memberWithId?.id);
                 return memberWithId.id;
             }
-            
+
         } catch (err) {
             throw new Error('Error');
         }
     }
+    async getBySomething(member: Partial<Omit<IMembers, 'id'>>) {
+        let somethingKey = null;
+        let somethingValue = null;
 
-    
+        for (const [key, value] of Object.entries(member)) {
+            if (value) {
+                somethingKey = key;
+                somethingValue = value;
+                break;
+            }
+        }
+
+        if (!somethingKey) throw new Error(`Pass a key to use as filter`);
+
+        const members = await this.kxConnection< IMembers >(this.tableName)
+            .where(somethingKey, '=', somethingValue)
+            .select(`*`);
+            
+        return members;
+        // if( response.length > 1 ) throw new Error(`More than one member!`);
+    }
+}
+const memberService = new MemberService();
+
+export {
+    memberService
 }
