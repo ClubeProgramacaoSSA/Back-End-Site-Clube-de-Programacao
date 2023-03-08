95b491d395bfc16a6b023e90a827fe8e353064d2
orlandomotapires <orlandomota2002@hotmail.com>
2023-03-08 13:23:56 -0300

    teste

--- src/Db/seeds/3-create_status_activities_seed.ts
+++ src/Db/seeds/3-create_status_activities_seed.ts
@@ -1,24 +1,24 @@
-// import { Knex } from "knex";
+import { Knex } from "knex";
 
-// const tableName = 'status_activities';
+const tableName = 'status_activities';
 
-// export async function seed(knex: Knex): Promise<void> {
-//     // Deletes ALL existing entries
-//     await knex(tableName).del();
+export async function seed(knex: Knex): Promise<void> {
+    // Deletes ALL existing entries
+    await knex(tableName).del();
 
-//     // Inserts seed entries
-//     await knex(tableName).insert([
-//         {            
-//             name: "Concluido",
-//         },
+    // Inserts seed entries
+    await knex(tableName).insert([
+        {            
+            name: "Concluido",
+        },
 
-//         {          
-//             name: "Em desenvolvimento",
-//         },
+        {          
+            name: "Em desenvolvimento",
+        },
 
-//         {          
-//             name: "Nao iniciado",
-//         }
+        {          
+            name: "Nao iniciado",
+        }
        
-//     ]);
-// };
+    ]);
+};
--- src/Db/seeds/4-create_member_seed.ts
+++ src/Db/seeds/4-create_member_seed.ts
@@ -1,57 +1,57 @@
-// import { Knex } from "knex";
+import { Knex } from "knex";
 
-// const tableName = 'members';
+const tableName = 'members';
 
-// export async function seed(knex: Knex): Promise<void> {
-//     // Deletes ALL existing entries
-//     await knex(tableName).del();
+export async function seed(knex: Knex): Promise<void> {
+    // Deletes ALL existing entries
+    await knex(tableName).del();
 
-//     // Inserts seed entries
-//     await knex(tableName).insert([
-//         { 
-//             born_at: "2000-10-27",
-//             started_course_at: "2000-10-27",
-//             updated_at: "2000-10-27",
-//             gender: "male",
-//             description: "hahaha",
-//             fullname: "Pedro Felipe Facundes"
-//         },
+    // Inserts seed entries
+    await knex(tableName).insert([
+        { 
+            born_at: "2000-10-27",
+            started_course_at: "2000-10-27",
+            updated_at: "2000-10-27",
+            gender: "male",
+            description: "hahaha",
+            fullname: "Pedro Felipe Facundes"
+        },
 
-//         { 
-//             born_at: "2000-10-27",
-//             started_course_at: "2000-10-27",
-//             updated_at: "2000-10-27",
-//             gender: "male",
-//             description: "hahaha",
-//             fullname: "Orlando Mota Pires"
-//         },
+        { 
+            born_at: "2000-10-27",
+            started_course_at: "2000-10-27",
+            updated_at: "2000-10-27",
+            gender: "male",
+            description: "hahaha",
+            fullname: "Orlando Mota Pires"
+        },
 
-//         { 
-//             born_at: "2000-10-27",
-//             started_course_at: "2000-10-27",
-//             updated_at: "2000-10-27",
-//             gender: "male",
-//             description: "hahaha",
-//             fullname: "Fernando Antonio Schettini"
-//         },
+        { 
+            born_at: "2000-10-27",
+            started_course_at: "2000-10-27",
+            updated_at: "2000-10-27",
+            gender: "male",
+            description: "hahaha",
+            fullname: "Fernando Antonio Schettini"
+        },
 
-//         { 
-//             born_at: "2000-10-27",
-//             started_course_at: "2000-10-27",
-//             updated_at: "2000-10-27",
-//             gender: "male",
-//             description: "hahaha",
-//             fullname: "Antonio Horacio Magalhaes"
-//         },
+        { 
+            born_at: "2000-10-27",
+            started_course_at: "2000-10-27",
+            updated_at: "2000-10-27",
+            gender: "male",
+            description: "hahaha",
+            fullname: "Antonio Horacio Magalhaes"
+        },
 
-//         { 
-//             born_at: "2000-10-27",
-//             started_course_at: "2000-10-27",
-//             updated_at: "2000-10-27",
-//             gender: "male",
-//             description: "hahaha",
-//             fullname: "Adrian Widmer"
-//         }
+        { 
+            born_at: "2000-10-27",
+            started_course_at: "2000-10-27",
+            updated_at: "2000-10-27",
+            gender: "male",
+            description: "hahaha",
+            fullname: "Adrian Widmer"
+        }
         
-//     ]);
-// };
+    ]);
+};
